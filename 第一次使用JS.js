8c80f575a865bb78c5dcafe23f1da11427787f6e
liquidsax <107411955+liquidsax@users.noreply.github.com>
2023-10-31 21:14:00 +0800

    Create 第一次使用JS.js
    以console为例开始使用Js吧！

---
+++ 第一次使用JS.js
@@ -0,0 +1,18 @@
+ console.log("i like ass");
+ console.log("it is really asshot");
+
+ window.alert("welcome to my ass !")
+/*
+console() 是 JavaScript 中的一个内置对象，用于输出信息到控制台。console() 对象提供了一系列方法来输出文本、数字、对象、数组等。
+
+console() 对象的常用方法包括：
+
+log()：输出文本到控制台。
+info()：输出信息到控制台。
+warn()：输出警告到控制台。
+error()：输出错误到控制台。
+dir()：输出对象的结构到控制台。
+table()：输出表格到控制台。
+time()：开始计时。
+timeEnd()：结束计时并输出结果到控制台。
+*/

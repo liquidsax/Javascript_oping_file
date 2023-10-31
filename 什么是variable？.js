7e313671ebc32e4d981328d8b05c9dfc8cc74f5f
liquidsax <107411955+liquidsax@users.noreply.github.com>
2023-10-31 21:16:32 +0800

    Create 什么是variable？.js
    在console里展示的内容要按下F12才看得见！

---
+++ 什么是variable？.js
@@ -0,0 +1,16 @@
+let age;//定义一个variable
+ age = 21;
+age = age +1;
+console.log("your are",age,"years old");
+let firstName = "Hana";
+console.log(firstName);
+let student = true;
+console.log(student);
+/*
+    在 JavaScript 中，let 是一个关键字，用于声明变量。let 声明的变量具有以下特点：
+-------------------------
+块级作用域：let 声明的变量只在声明语句所在的代码块内有效。
+可重复声明：let 声明的变量可以在同一个作用域内多次声明，但每次声明的变量必须是不同的。
+变量提升：let 声明的变量不会发生变量提升，也就是说，变量必须在声明语句执行后才能被访问。
+let 是 ES6 新增的关键字，用于取代 var 关键字。let 的设计目的是解决 var 关键字的一些缺陷，例如变量提升、全局变量污染等。
+*/
